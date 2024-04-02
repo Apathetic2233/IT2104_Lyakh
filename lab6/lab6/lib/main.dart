9cb0532b03dbb8116136bad860319faab8d63f9e
Apathetic2233 <fedirko2003@gmail.com>
2024-04-02 08:50:18 +0300

    Создание формы и введение полей ввода

--- lab6/lab6/lib/main.dart
+++ lab6/lab6/lib/main.dart
@@ -9,58 +9,36 @@ class MyForm extends StatefulWidget {
 
 class MyFormState extends State {
   final _formKey = GlobalKey<FormState>();
-  int? _width;
-  int? _height;
+ 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            const Text('Ширина(мм):', style: TextStyle(fontSize: 20.0)),
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) return 'Пожалуйста введите ширину';
-              },
-              onSaved: (value) {
-                _width = int.parse(value!);
-              },
-            ),
-            const SizedBox(height: 20.0),
-            const Text('Высота(мм):', style: TextStyle(fontSize: 20.0)),
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) return 'Пожалуйста введите высоту';
-              },
-              onSaved: (value) {
-                _height = int.parse(value!);
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Вычисление прошло успешно!'),backgroundColor: Colors.green,),);
-                }
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  setState(() {});
-                }
-              },
-              child: const Text('Вычислить', style: TextStyle(color: Colors.white)),
-            ),
-            SizedBox(height: 50.0),
-            Center(
-              child: Text(
-                _width == null || _height == null ? 'Введите значения' : 'S = $_width * $_height = ${_width! * _height!} (мм2)',
-                style: TextStyle(fontSize: 30.0),
-              ),
-            ),
-          ],
-        ),
+    return Container(padding: const EdgeInsets.all(10.0),
+    child: Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+      const Text('Ширина(мм):', style: TextStyle(fontSize: 20.0),),
+      TextFormField(validator: (value){
+    if (value!.isEmpty) return 'Пожалуйста введите ширину';
+      }),
+      const SizedBox(height: 20.0),
+
+          const Text('Длину(мм):', style: TextStyle(fontSize: 20.0),),
+          TextFormField(validator: (value){
+            if (value!.isEmpty) return 'Пожалуйста введите длину';
+          }),
+
+          ElevatedButton(onPressed: (){
+        if (_formKey.currentState!.validate()) {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Вычисление прошло успешно!'),backgroundColor: Colors.green,),);
+        }
+      },
+        child: const Text ('Вычислить', style: TextStyle(color: Colors.white),)
       ),
+      ],
+      ),
+    ),
     );
   }
 }
@@ -69,8 +47,8 @@ void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      appBar: AppBar(title: const Text('Калькулятор площади')),
-      body: const MyForm(),
-    ),
-  ),
+      appBar: AppBar(title: const Text('Калькулятор площади ')),
+      body: const MyForm()
+    )
+  )
 );
